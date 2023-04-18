73537ddd7f41c336b55853647f592b47a3ca2543
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-04-18 19:12:26 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,19 @@ import SearchExercise from '../components/SearchExercises';
 import Exercises from '../components/Exercises';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
   return (
     <Box>
       <HeroBanner />
-      <SearchExercise />
-      <Exercises />
+      <SearchExercise 
+      setExercises={setExercises}
+      bodyPart={bodyPart}
+      setBodyPart={setBodyPart} />
+      <Exercises 
+      setExercises={setExercises}
+      bodyPart={bodyPart}
+      setBodyPart={setBodyPart} />
       
     </Box>
   )
