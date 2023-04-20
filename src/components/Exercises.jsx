f3d75481f6f1dbf8acebe2b726b0b7fe931160c6
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-04-20 15:37:04 +0530

    commit

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
-
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-   <div>
-    takeaway
-   </div>
+   <Box id="exercises"
+     sx={{ mt: {lg: '110px'}}}
+     mt= '50px'
+     p="20px"
+   >
+    <Typography variant='h3' mb='46px'>
+      Showing Results
+    </Typography>
+   </Box>
   )
 }
 
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,38 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-
-
-const SearchExercises = ({ setExercises, bodyPart, setBodyPart } ) => {
-  const [search, setSearch] = useState ('');
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState('');
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
-       const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
 
-       setBodyParts(['all', ...bodyPartsData]);
-    }
+      setBodyParts(['all', ...bodyPartsData]);
+    };
 
     fetchExercisesData();
-  },[])
+  }, []);
 
   const handleSearch = async () => {
-    if(search) {
+    if (search) {
       const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
 
-      const searchedExercises = exercisesData.filter((exercise) => exercise.name.toLowerCase().includes(search) 
-      || exercise.target.toLowerCase().includes(search)
-      || exercise.equipment.toLowerCase().includes(search)
-      || exercise.bodyPart.toLowerCase().includes(search)
+      const searchedExercises = exercisesData.filter(
+        (item) => item.name.toLowerCase().includes(search)
+               || item.target.toLowerCase().includes(search)
+               || item.equipment.toLowerCase().includes(search)
+               || item.bodyPart.toLowerCase().includes(search),
       );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
 
       setSearch('');
       setExercises(searchedExercises);
     }
-  }
+  };
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
@@ -48,14 +50,12 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart } ) => {
           placeholder="Search Exercises"
           type="text"
         />
-        <Button className="search-btn" sx={{ bgcolor: '#b18686', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }}
-        onClick={handleSearch} >
+        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
           Search
         </Button>
       </Box>
-      <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
-        <HorizontalScrollbar data={bodyParts}
-        bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+        <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
       </Box>
     </Stack>
   );
